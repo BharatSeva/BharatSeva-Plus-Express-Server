4421c1c798d7af2688fcd715f7b392fa6748e95b
Vaibhav Yadav <tron21vaibhav@gmail.com>
2023-06-12 18:59:33 +0530

    Firebase Added

--- Firebase/Service.js
+++ Firebase/Service.js
@@ -5,39 +5,86 @@ const { collection, getDocs, getDoc, addDoc, updateDoc, deleteDoc, doc } = requi
 
 const BharatCollectionRef = collection(db, "BharatSeva")
 
+
+const Getdata = async (HID) => {
+    const docRef = doc(db, "BharatSeva", HID);
+    const docSnap = await getDoc(docRef);
+    return docSnap.data();
+}
+
 const Update_Records = async (req, res) => {
-    console.log(req.body)
     const NewData = doc(db, "BharatSeva", req.body.HealthId)
-    updateDoc(NewData, req.body)
+    await updateDoc(NewData, req.body)
     res.status(200).json({ Status: "Successfull" })
 }
 
 const GetAllData = async (req, res) => {
-    // try {
-    // let data = await BharatCollectionRef.get()
-    // let Dataas = await data();
-    // console.log(doc.Dataas());
-    // BharatCollectionRef.get()
-    //     .then((qs)=>{
-    //         qs.forEach(element => {
-    //             var data = doc.data()
-    //             console.log(data)
-
-    //         });
-    //     })
-
-    
-    const DataG = await getDocs(BharatCollectionRef)
-    res.status(200).json({ Data: DataG })
-    // getDocs(BharatCollectionRef
-    // }
-    // catch(e) {
-    //     console.log(e.message)
-    //     res.status(400).json({ message: e.message })
-    // }
+    const docRef = doc(db, "BharatSeva", req.headers.health_id);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+        res.status(200).json({ status: "Success", Data: docSnap.data() })
+    } else {
+        res.status(200).json({ status: "Failed", message: "Could Not Find that Attribute" })
+    }
 }
+
+const Update_No_Of_Views = async (req, res) => {
+    let Total_Records = await Getdata(req.headers.health_id)
+    Total_Records = Total_Records.TotalNoOfViews
+    Total_Records += 1;
+    const docRef = doc(db, "BharatSeva", req.headers.health_id);
+    await updateDoc(docRef, { "TotalNoOfViews": Total_Records })
+    res.status(200).json({ status: "Success" })
+}
+
+const RecordsViewed = async(req, res)=>{ 
+    let RV = await Getdata(req.headers.health_id)
+    RV = RV.RecordsViewed
+    RV+=1;
+    const docRef = doc(db, "BharatSeva", req.headers.health_id);
+    await updateDoc(docRef, { "RecordsViewed": RV })
+    res.status(200).json({ status: "Success" })
+    
+}
+
+const HealthID_Created = async(req, res)=>{
+    let HID = await Getdata(req.headers.health_id)
+    HID = HID.HealthID_Created
+    HID+=1;
+    const docRef = doc(db, "BharatSeva", req.headers.health_id);
+    await updateDoc(docRef, { "HealthID_Created": HID })
+    res.status(200).json({ status: "Success" })
+    
+}
+
+const RecordsCreated = async(req, res)=>{
+    let HID = await Getdata(req.headers.health_id)
+    HID = HID.RecordsCreated
+    HID+=1;
+    const docRef = doc(db, "BharatSeva", req.headers.health_id);
+    await updateDoc(docRef, { "RecordsCreated": HID })
+    res.status(200).json({ status: "Success" })
+    
+}
+
+const BioDV = async(req, res)=>{
+    let HID = await Getdata(req.headers.health_id)
+    HID = HID.BioDV
+    HID+=1;
+    const docRef = doc(db, "BharatSeva", req.headers.health_id);
+    await updateDoc(docRef, { "BioDV": HID })
+    res.status(200).json({ status: "Success" })
+    
+}
+
+
 
 module.exports = {
     Update_Records,
-    GetAllData
+    GetAllData,
+    Update_No_Of_Views,
+    HealthID_Created,
+    RecordsViewed,
+    RecordsCreated,
+    BioDV
 } 
--- Router/FirebaseRouter.js
+++ Router/FirebaseRouter.js
@@ -1,11 +1,14 @@
 const express = require("express")
 const router = express.Router();
-const { Update_Records, GetAllData } = require("../Firebase/Service")
+const { Update_Records, GetAllData, Update_No_Of_Views, RecordsCreated, RecordsViewed, HealthID_Created, BioDV } = require("../Firebase/Service")
 
 
 
 router.post('/firebase/chpre', Update_Records)
 router.get('/firebase/GET', GetAllData)
-
+router.get('/firebase/BioDV', BioDV)
+router.get('/firebase/RecordsCreated', RecordsCreated)
+router.get('/firebase/RecordsViewed', RecordsViewed)
+router.get('/firebase/HealthID_Created', HealthID_Created)
 
 module.exports = router
